65d2fb941ba749f621d433a57c63c7338d5a0334
Anna Klingauf <a.klingauf@micromata.de>
2024-06-06 20:58:14 +0200

    FE: Add simple tab navigation

--- app/Crimpy/App.js
+++ app/Crimpy/App.js
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { User } from "./src/pages/User";
+import { Session } from "./src/pages/Session";
+import { Gym } from "./src/pages/Gym";
+import { Analysis } from "./src/pages/Analysis";
+import { Feather } from "@expo/vector-icons";
+import { Ionicons } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="User"
+          component={User}
+          options={{
+            tabBarShowLabel: false,
+            tabBarIcon: ({ color, size }) => (
+              <Feather name="user" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Session"
+          component={Session}
+          options={{
+            tabBarShowLabel: false,
+            tabBarIcon: ({ color, size }) => (
+              <Feather name="play-circle" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Gym"
+          component={Gym}
+          options={{
+            tabBarShowLabel: false,
+            tabBarIcon: ({ color, size }) => (
+              <Feather name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Analysis"
+          component={Analysis}
+          options={{
+            tabBarShowLabel: false,
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="analytics" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ app/Crimpy/src/pages/Gym.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { ScrollView, Text } from "react-native";
+
+export function Gym() {
+  return (
+    <ScrollView>
+      <Text>Gym</Text>
+      <Text>This is the gym page</Text>
+    </ScrollView>
+  );
+}
